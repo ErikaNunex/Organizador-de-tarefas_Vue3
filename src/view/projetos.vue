ac20f8666b4fa8f02dcb4ab7d081fa90137aee2f
ErikaNunes <erikanunes0512@gmail.com>
2023-04-04 17:43:27 -0300

    implementando vuex

--- src/view/projetos.vue
+++ src/view/projetos.vue
@@ -32,26 +32,28 @@
   </section>
 </template>
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
-import IProjeto from "../interfaces/IProjeto";
+import { defineComponent, computed } from "vue";
+import { useStore } from "@/store";
 
 export default defineComponent({
   name: "ProjetosView",
   data() {
     return {
       nomeDoProjeto: "",
-      projetos: [] as IProjeto[],
     };
   },
   methods: {
     salvar() {
-      const projeto: IProjeto = {
-        nome: this.nomeDoProjeto,
-        id: new Date().toISOString(),
-      };
-      this.projetos.push(projeto);
+      this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
       this.nomeDoProjeto = "";
     },
+  },
+  setup() {
+    const store = useStore();
+    return {
+      store,
+      projetos: computed(() => store.state.projetos),
+    };
   },
 });
 </script>
